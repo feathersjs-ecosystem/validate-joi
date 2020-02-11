d9e3dabfb863ef05cc2e8177863fd3480a7b39d0
Marshall Thompson <marshall@creativeideal.net>
2020-02-11 15:55:16 -0700

    Rewrite with latest @hapi/joi

--- index.js
+++ index.js
@@ -1,42 +1,66 @@
-
 /* eslint comma-dangle: 0, object-shorthand: 0, prefer-arrow-callback: 0*/ /* ES5 code */
 const errors = require('@feathersjs/errors');
 const utils = require('feathers-hooks-common/lib/services');
 const joiErrorsForForms = require('joi-errors-for-forms');
 
-function validator(joiSchema, joiOptions, translator, ifTest) {
-  return function validatorInner(hook, next) {
-    utils.checkContext(hook, 'before', ['create', 'update', 'patch'], 'validate-joi');
-    const values = utils.getItems(hook);
+// We only directly need the convert option. The others are listed for convenience.
+// See defaults at https://hapi.dev/family/joi/api/?v=17.1.0#anyvalidatevalue-options
+const joiDefaults = {
+  abortEarly: true,
+  allowUnknown: false,
+  cache: true,
+  convert: true,
+  debug: false,
+  externals: true,
+  noDefaults: false,
+  nonEnumerables: false,
+  presence: 'optional',
+  skipFunctions: false,
+  stripUnknown: false
+};
 
-    Joi.validate(values, joiSchema, joiOptions,
-      function (joiErr, convertedValues) {
-        const formErrors = translator(joiErr);
-        if (formErrors) {
-          // Hacky, but how else without a custom assert?
-          const msg = ifTest ? JSON.stringify(formErrors) : 'Invalid data';
-          throw new errors.BadRequest(msg, { errors: formErrors });
-        }
+function setupValidateWithJoi(joiSchema, joiOptions, translator, ifTest) {
+  if (!['undefined', 'object'].includes(typeof joiOptions)) {
+    throw new errors.GeneralError('joiOptions must be a valid object.');
+  }
 
-        if (!joiOptions || joiOptions.convert === true) {
-          utils.replaceItems(hook, convertedValues);
-        }
-        
-        if (typeof next === 'function') {
-          next(null, hook);
-        }
+  const mergedOptions = Object.assign({}, joiDefaults, joiOptions);
+
+  return async function validateWithJoi(context, next) {
+    utils.checkContext(context, 'before', ['create', 'update', 'patch'], 'validate-joi');
+    const values = utils.getItems(context);
+
+
+    try {
+      const convertedValues = await joiSchema.validateAsync(values, mergedOptions);
+
+      if (mergedOptions.convert === true) {
+        utils.replaceItems(context, convertedValues);
       }
-    );
+
+      if (typeof next === 'function') {
+        return next(null, context);
+      }
+      return context;
+    } catch (error) {
+      const formErrors = translator(error);
+      if (formErrors) {
+        // Hacky, but how else without a custom assert?
+        const msg = ifTest ? JSON.stringify(formErrors) : 'Invalid data';
+        throw new errors.BadRequest(msg, { errors: formErrors });
+      }
+      return formErrors || error;
+    }
   };
 }
 
 module.exports = {
   form: function (joiSchema, joiOptions, translations, ifTest) {
     const translator = joiErrorsForForms.form(translations);
-    return validator(joiSchema, joiOptions, translator, ifTest);
+    return setupValidateWithJoi(joiSchema, joiOptions, translator, ifTest);
   },
   mongoose: function (joiSchema, joiOptions, translations, ifTest) {
     const translator = joiErrorsForForms.mongoose(translations);
-    return validator(joiSchema, joiOptions, translator, ifTest);
+    return setupValidateWithJoi(joiSchema, joiOptions, translator, ifTest);
   }
 };
